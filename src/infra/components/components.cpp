eceb17c59361c087e1048d8fcb7ddf9370b60f41
Ilya Repin <ilya-r04@yandex.ru>
2025-12-19 19:15:11 +0000

    add monitoring

--- src/infra/components/components.cpp
+++ src/infra/components/components.cpp
@@ -14,6 +14,7 @@
 #include <userver/components/component_list.hpp>
 #include <userver/congestion_control/component.hpp>
 #include <userver/server/handlers/ping.hpp>
+#include <userver/server/handlers/server_monitor.hpp>
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
@@ -25,6 +26,7 @@ void RegisterUserverComponents(userver::components::ComponentList& list) {
       .Append<userver::components::TestsuiteSupport>()
       .Append<userver::components::HttpClient>()
       .Append<userver::clients::dns::Component>()
+      .Append<userver::server::handlers::ServerMonitor>()
       .Append<userver::server::handlers::TestsControl>()
       .Append<userver::congestion_control::Component>();
 }

3ca1f8cc9fc5dad234230e626de75aa091f201a5
Daniil Repin <daniil.r4@yandex.ru>
2025-12-22 10:18:24 +0000

    yet another fix

--- src/app/use-cases/mocks/user_repo_mock.hpp
+++ src/app/use-cases/mocks/user_repo_mock.hpp
@@ -10,6 +10,10 @@ using namespace NChat::NCore;
 class MockUserRepository : public IUserRepository {
  public:
   MOCK_METHOD(void, InsertNewUser, (const TUser& user), (const, override));
+  MOCK_METHOD(void, DeleteUser, (std::string_view username), (const, override));
+  MOCK_METHOD(std::string, UpdateUser, (const TUsername& username_to_update, const TUserUpdateParams& params),
+              (const, override));
+  MOCK_METHOD(std::optional<TUserId>, FindByUsername, (std::string_view username), (const, override));
   MOCK_METHOD(std::optional<TUserTinyProfile>, GetProfileById, (const TUserId& user_id), (const, override));
   MOCK_METHOD(std::unique_ptr<TUser>, GetUserByUsername, (std::string_view username), (const, override));
 };

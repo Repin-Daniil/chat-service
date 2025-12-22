35fe10a1c6c3e6be8bc11c898e64b45118e13498
Daniil Repin <daniil.r4@yandex.ru>
2025-12-22 09:52:33 +0000

    Refactoring

--- src/app/use-cases/mocks/auth_service_mock.hpp
+++ src/app/use-cases/mocks/auth_service_mock.hpp
@@ -1,20 +1,11 @@
+#pragma once
+
 #include <core/users/auth_service_interface.hpp>
-#include <core/users/user_repo.hpp>
 
 #include <gmock/gmock.h>
 
 using namespace testing;
 using namespace NChat::NCore;
-
-// Моки для интеграционного теста
-class MockUserRepository : public IUserRepository {
- public:
-  MOCK_METHOD(void, InsertNewUser, (const TUser& user), (const, override));
-  MOCK_METHOD(void, DeleteUser, (std::string_view username), (const, override));
-  MOCK_METHOD(std::optional<TUserId>, FindByUsername, (std::string_view username), (const, override));
-  MOCK_METHOD(std::optional<TUserTinyProfile>, GetProfileById, (const TUserId& user_id), (const, override));
-  MOCK_METHOD(std::unique_ptr<TUser>, GetUserByUsername, (std::string_view username), (const, override));
-};
 
 class MockAuthService : public IAuthService {
  public:

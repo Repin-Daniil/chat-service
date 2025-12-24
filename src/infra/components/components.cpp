1bdd99bc05309731425fadfe961dd45d8855e4c2
Daniil Repin <daniil.r4@yandex.ru>
2025-12-24 11:08:03 +0000

    Update to v2.14

--- src/infra/components/components.cpp
+++ src/infra/components/components.cpp
@@ -16,15 +16,15 @@
 #include <api/http/v1/users/user_register_handler.hpp>
 
 #include <userver/clients/dns/component.hpp>
-#include <userver/clients/http/component.hpp>
+#include <userver/clients/http/component_list.hpp>
 #include <userver/components/component.hpp>
 #include <userver/components/component_list.hpp>
 #include <userver/congestion_control/component.hpp>
-#include <userver/dynamic_config/client/component.hpp>
-#include <userver/dynamic_config/updater/component.hpp>
+#include <userver/dynamic_config/updater/component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/server_monitor.hpp>
 #include <userver/server/handlers/tests_control.hpp>
+#include <userver/server/middlewares/cors.hpp>
 #include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 
@@ -32,14 +32,14 @@ namespace NChat::NInfra {
 
 void RegisterUserverComponents(userver::components::ComponentList& list) {
   list.Append<userver::server::handlers::Ping>()
+      .AppendComponentList(userver::clients::http::ComponentList())
+      .AppendComponentList(userver::dynamic_config::updater::ComponentList())
       .Append<userver::components::TestsuiteSupport>()
-      .Append<userver::components::HttpClient>()
       .Append<userver::clients::dns::Component>()
       .Append<userver::server::handlers::ServerMonitor>()
       .Append<userver::server::handlers::TestsControl>()
       .Append<userver::congestion_control::Component>()
-      .Append<userver::components::DynamicConfigClientUpdater>()
-      .Append<userver::components::DynamicConfigClient>();
+      .Append<userver::server::middlewares::CorsFactory>();
 }
 
 void RegisterAuthCheckerFactory() {

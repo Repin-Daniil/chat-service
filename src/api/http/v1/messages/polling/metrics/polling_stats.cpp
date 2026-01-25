cc3b2f87ea3bbe6d5a6b723a1b92928158a4da45
Daniil Repin <daniil.r4@yandex.ru>
2026-01-25 08:41:03 +0000

    Metrics update

--- src/api/http/v1/messages/polling/metrics/polling_stats.cpp
+++ src/api/http/v1/messages/polling/metrics/polling_stats.cpp
@@ -3,12 +3,12 @@
 namespace NChat::NInfra {
 
 void DumpMetric(userver::utils::statistics::Writer& writer, const TPollingStatistics& stats) {
-  writer["active"]["current"] = stats.active_polling_amount;
-  writer["polling_duration"]["sec"]["hist"] = stats.polling_duration_sec_hist;
+  writer["polling"]["active"]["current"] = stats.active_polling_amount;
+  writer["polling"]["duration"]["sec"]["hist"] = stats.polling_duration_sec_hist;
   writer["resync_required"]["total"] = stats.resync_required_total;
   writer["send_overhead"]["us"]["hist"] = stats.send_overhead_us_hist;
   writer["queue_wait_latency"]["sec"]["hist"] = stats.queue_wait_latency_sec_hist;
-  writer["polling_overhead"]["us"]["hist"] = stats.polling_overhead_us_hist;
+  writer["polling"]["overhead"]["us"]["hist"] = stats.polling_overhead_us_hist;
   writer["batch"]["size"]["hist"] = stats.batch_size_hist;
 }
 
--- src/infra/messaging/sessions/metrics/sessions_stats.cpp
+++ src/infra/messaging/sessions/metrics/sessions_stats.cpp
@@ -5,7 +5,7 @@ namespace NChat::NInfra {
 void DumpMetric(userver::utils::statistics::Writer& writer, const TSessionsStatistics& stats) {
   writer["opened"]["current"] = stats.opened_sessions_current;
   writer["removed"]["total"] = stats.removed_sessions_total;
-  writer["messages"]["total"] = stats.messages_sent_total;
+  writer["messages"]["sent"]["total"] = stats.messages_sent_total;
   writer["per_user"]["hist"] = stats.sessions_per_user_hist;
   writer["queue"]["size"]["hist"] = stats.queue_size_hist;
   writer["lifetime"]["sec"]["hist"] = stats.lifetime_sec_hist;
